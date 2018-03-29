8c6c329d9aeaee6c9f908bd2cef6f06a9bcd22cd
Salvador Guzman <guzmansalv@gmail.com>
2018-03-29 04:11:59 -0700

    simple hello

--- src/main/scala/com/github/sguzman/js/go/Main.scala
+++ src/main/scala/com/github/sguzman/js/go/Main.scala
@@ -1,7 +1,8 @@
 package com.github.sguzman.js.go
 
-import com.thoughtworks.binding.dom
+import com.thoughtworks.binding.{Binding, dom}
 import org.scalajs.dom.document
+import org.scalajs.dom.html.Div
 
 object Main {
   @dom def _render: com.thoughtworks.binding.Binding[org.scalajs.dom.html.Div] = {
@@ -14,7 +15,7 @@ object Main {
     </div>
   }
 
-  def render = _render
+  def render: Binding[Div] = _render
 
   def main(args: Array[String]): Unit = {
     com.thoughtworks.binding.dom.render(document.body, render)
